08a12c5f6173293db7cdd0917e4641a6d37183f5
Oyebode <adetunji.oyebode6@gmail.com>
2023-02-10 17:20:41 +0100

    commit update basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -1,10 +1,20 @@
 #!/usr/bin/python3
 import uuid
 from datetime import datetime
+"""
+    Representation of class BaseModel
+"""
 
 
 class BaseModel():
+    """
+        A Class that Represent a BaseModel
+    """
     def __init__(self, *args, **kwargs):
+        """
+            **kwargs:
+                Initializes the attributes of kwargs of BaseModel
+        """
         if len(kwargs) != 0:
             for key, value in kwargs.items():
                 if key == "__class__":
@@ -20,12 +30,22 @@ class BaseModel():
             self.updated_at = datetime.now()
 
     def __str__(self):
+        """
+            A module in BaseModel that enables printing \
+            of instances of the class
+        """
         return f"[{__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
+        """
+            intances are updated when it is created
+        """
         self.updated_at = datetime.now()
 
     def to_dict(self):
+        """
+            creates a copy of the dictionary of BaseModel
+        """
         dict_2 = self.__dict__.copy()
         dict_2["__class__"] = self.__class__.__name__
         dict_2["created_at"] = self.created_at.isoformat()

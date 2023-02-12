9da708852312e9ffa9a8c07f68ada2814ac52845
Oyebode <adetunji.oyebode6@gmail.com>
2023-02-12 08:44:13 +0100

    task3

--- models/base_model.py
+++ models/base_model.py
@@ -10,6 +10,7 @@ class BaseModel():
     def __init__(self):
         """ Initializes the attributes of the class
         """
+
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
@@ -17,7 +18,7 @@ class BaseModel():
     def __str__(self):
         """ Defines a string representation of the instance """
 
-        return f"[{__class__.__name__}] ({self.id}) {self.__dict__}"
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
         """ updates the public instance attribute `updated_at` \

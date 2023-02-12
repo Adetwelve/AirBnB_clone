fe294e571c5cf52b2b82fca9d469fcb838337ea0
Oyebode <adetunji.oyebode6@gmail.com>
2023-02-12 08:19:19 +0100

    task3

--- models/base_model.py
+++ models/base_model.py
@@ -1,45 +1,38 @@
 #!/usr/bin/python3
 import uuid
 from datetime import datetime
-"""
-    Representation of class 'BaseModel'
-"""
+""" Representation of class `BaseModel` """
 
 
 class BaseModel():
-    """
-        Defines all common attributes/method of the class
-    """
+    """ Defines all common attributes/method of the class """
 
     def __init__(self):
-        """
-            Initializes the attributes of the class
+        """ Initializes the attributes of the class
         """
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
     def __str__(self):
-        """
-            Defines a string representation of the instance
-        """
+        """ Defines a string representation of the instance """
 
         return f"[{__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
-        """
-            intances are updated when it is created
+        """ updates the public instance attribute `updated_at` \
+            with the current datetime
         """
 
         self.updated_at = datetime.now()
 
     def to_dict(self):
-        """
-            creates a copy of the dictionary of BaseModel
+        """ returns a dictionary containing all keys/value \
+            of `__dict__` of the instance
         """
 
-        dict_2 = self.__dict__.copy()
-        dict_2["__class__"] = self.__class__.__name__
-        dict_2["created_at"] = self.created_at.isoformat()
-        dict_2["updated_at"] = self.updated_at.isoformat()
-        return dict_2
+        dict_copy = self.__dict__.copy()
+        dict_copy["__class__"] = self.__class__.__name__
+        dict_copy["created_at"] = self.created_at.isoformat()
+        dict_copy["updated_at"] = self.updated_at.isoformat()
+        return dict_copy

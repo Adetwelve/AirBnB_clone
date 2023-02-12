e2ce9151562619953803eb4303b88ac3c8c3b9b0
Oyebode <adetunji.oyebode6@gmail.com>
2023-02-12 06:59:24 +0100

    task5

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import uuid
 from datetime import datetime
+import models
 """
     Representation of class BaseModel
 """
@@ -28,6 +29,7 @@ class BaseModel():
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            models.storage.new(self)
 
     def __str__(self):
         """
@@ -41,6 +43,7 @@ class BaseModel():
             intances are updated when it is created
         """
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import json
+from models.base_model import BaseModel
 """ 
     Write a class FileStorage that serializes instances \
     to a JSON file and deserializes JSON file to instances:
@@ -22,7 +23,16 @@ class FileStorage():
 
     def save(self):
         with open(self.__file_path, 'w', encoding="utf-8") as f:
-            j_file = {k : V.to_dict() for K, v in self.__object.items()}
+            j_file = {k : v.to_dict() for k, v in self.__objects.items()}
             json.dump(j_file, f)
 
-
+    def reload(self): 
+        try:
+            with open(self.__file_path, 'r', encoding="utf-8    ") as f:
+                obj_dict = json.load(f)
+                for o in obj_dict.values():
+                    cls_name = o["__class__"]
+                    del o["__class__"]
+                    self.new(eval(cls_name)(**o))
+        except FileNotFoundError:
+            return
